42902ce2ce4251dc31af7c03076ebc6451a6d4c8
sachindaingade <sbdaingade@gmail.com>
2026-07-11 09:42:49 +0530

    first

---
+++ SwiftDataProject/SwiftDataProject/SwiftDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataProjectApp.swift
+//  SwiftDataProject
+//
+//  Created by Sachin Daingade on 11/07/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftDataProject/SwiftDataProjectTests/SwiftDataProjectTests.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftDataProjectTests.swift
+//  SwiftDataProjectTests
+//
+//  Created by Sachin Daingade on 11/07/26.
+//
+
+import Testing
+
+struct SwiftDataProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
